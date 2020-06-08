2afb549b05ef695e207a516ae3ef7f326482d0f4
maosan132 <maosan132@gmail.com>
2020-06-08 17:45:20 -0500

    added complete_name method'

--- contact.rb
+++ contact.rb
@@ -15,10 +15,22 @@ class Contact
   def last_name
     @last_name
   end
+
+  def complete_name
+    complete_name = first_name
+    complete_name << ' '
+    complete_name.nil? ? complete_name = ' ' : complete_name << middle_name
+    complete_name << ' '
+    complete_name << last_name
+  end
+
 end
 
 mauricio = Contact.new
 mauricio.first_name = 'Andrés'
 mauricio.middle_name = 'Santos'
+mauricio.last_name = 'hoyos'
 puts mauricio.first_name
 puts mauricio.middle_name
+
+puts mauricio.complete_name
