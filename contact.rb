004bd4bb8611cbd91bf864c4066c5cc567a902cb
maosan132 <maosan132@gmail.com>
2020-06-09 16:20:36 -0500

    added phone class

--- contact.rb
+++ contact.rb
@@ -18,11 +18,14 @@ class Contact
   end
 
   def complete_name
-    complete_name = 'first_name'
+    complete_name = first_name
     complete_name << ' '
     middle_name.nil? ? complete_name << '' : complete_name << middle_name
     complete_name << ' ' << last_name
+  end
 
+  def first_last
+    first_name + ' ' + last_name
   end
 
   def last_first
@@ -33,16 +36,41 @@ class Contact
     middle_name.nil? ? last_first = '' : last_first << middle_name.slice(0, 1) <<'.'
     
   end
+
+  def to_s(format = 'full')
+    case format
+    when 'full'
+      complete_name
+    when 'last_first'
+      last_first
+    when 'first'
+      first_name
+    when 'last'
+      last_name
+    else
+      first_last
+    end
+  end
 end
 
 mauricio = Contact.new
 mauricio.first_name = 'Andrés'
-puts mauricio.first_name + ' 1'
 mauricio.middle_name = 'Santos'
 mauricio.last_name = 'hoyos'
 print 'I am '
+puts mauricio.to_s
+puts mauricio.to_s('first')
 puts mauricio.first_name + mauricio.middle_name + mauricio.last_name
-puts mauricio.first_name + ' 2 '
 puts mauricio.complete_name
-puts mauricio.first_name + ' 3 '
 puts mauricio.last_first
+puts
+jorge = Contact.new
+jorge.first_name = 'Andrés'
+jorge.middle_name = 'Santos'
+jorge.last_name = 'hoyos'
+print 'I am '
+puts jorge.to_s
+puts jorge.to_s('first')
+puts jorge.first_name + jorge.middle_name + jorge.last_name
+puts jorge.complete_name
+puts jorge.last_first
---
+++ phone_numbers.rb
@@ -0,0 +1,8 @@
+class PhoneNumber
+  atter_accessor :kind, :number
+
+  def to_s
+    #{kind}: #{number}
+  end
+
+end
