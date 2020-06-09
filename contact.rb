c318a534efbd270a7bbdd19f50e2b5ebc2718894
maosan132 <maosan132@gmail.com>
2020-06-09 15:19:28 -0500

    resolved a conflict betwween variable first_name and method copmplete_name

--- contact.rb
+++ contact.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Contact
 
    
@@ -17,20 +18,31 @@ class Contact
   end
 
   def complete_name
-    complete_name = first_name
+    complete_name = 'first_name'
     complete_name << ' '
-    complete_name.nil? ? complete_name = ' ' : complete_name << middle_name
-    complete_name << ' '
-    complete_name << last_name
+    middle_name.nil? ? complete_name << '' : complete_name << middle_name
+    complete_name << ' ' << last_name
+
   end
 
+  def last_first
+    last_first = last_name
+    last_first << ', '
+    last_first << first_name
+    last_first << ' '
+    middle_name.nil? ? last_first = '' : last_first << middle_name.slice(0, 1) <<'.'
+    
+  end
 end
 
 mauricio = Contact.new
 mauricio.first_name = 'Andrés'
+puts mauricio.first_name + ' 1'
 mauricio.middle_name = 'Santos'
 mauricio.last_name = 'hoyos'
-puts mauricio.first_name
-puts mauricio.middle_name
-
+print 'I am '
+puts mauricio.first_name + mauricio.middle_name + mauricio.last_name
+puts mauricio.first_name + ' 2 '
 puts mauricio.complete_name
+puts mauricio.first_name + ' 3 '
+puts mauricio.last_first
